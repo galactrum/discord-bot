aff567f8dc28fdbde5a815649dd986441d2337e9
Aareon Sullivan <askully13@gmail.com>
2017-01-30 12:47:15 -0800

    Update wallet_info.py

--- wallet_info.py
+++ wallet_info.py
@@ -10,7 +10,7 @@ def rpcdat(method,params,port):
             "params": params,
             "port": port
             })
-        req = requests.get('http://127.0.0.1:'+port, data=rpcdata, auth=('user', 'pass'), timeout=8)
+        req = requests.get('http://127.0.0.1:'+port, data=rpcdata, auth=('srf2UUR0', 'srf2UUR0XomxYkWw'), timeout=8)
         return req.json()['result']
     except Exception as e:
         return "Error: "+str(e)
@@ -23,15 +23,19 @@ class wallet:
     async def wallet(self):
         """Shows wallet info"""
         port =  "11311"
-        wallet_balance = rpcdat('getbalance',[],port)
+        get_info = rpcdat('getinfo',[],port)
+        wallet_balance = float(get_info["balance"])+float(get_info["stake"])
         stake_info = rpcdat('getstakinginfo',[],port)
+        connection_count = rpcdat('getconnectioncount',[],port)
         stake_weight = stake_info["weight"]
         net_weight = stake_info["netstakeweight"]
         embed = discord.Embed(colour=discord.Colour.red())
-        embed.add_field(name="Wallet owner", value="Aareon")
-        embed.add_field(name="Balance", value=wallet_balance)
+        embed.add_field(name="Wallet owner", value="Aareon#0892")
+        embed.add_field(name="Balance (NET)", value=wallet_balance)
         embed.add_field(name="Staking Weight", value=stake_weight)
         embed.add_field(name="Net Stake Weight", value=net_weight)
+        embed.add_field(name="Connections", value=connection_count)
+        embed.set_footer(text="Sponsored by altcointrain.com - Choo!!! Choo!!!")
 
         try:
             await self.bot.say(embed=embed)
