5805a1641eeefca672348c45ed1f2f23c0bc8188
OrangeChaten <josejoshua@hotmail.co.uk>
2017-02-08 17:20:49 +0000

    Fixed your synactic sugar issues

--- deposit.py
+++ deposit.py
@@ -2,18 +2,19 @@ import discord, json, requests
 from discord.ext import commands
 from cogs.utils import rpc_module as rpc
 
-rpc = rpc.Rpc
+
 
 class deposit:
     def __init__(self, bot):
         self.bot = bot
+        self.rpc = rpc.Rpc()
 
     @commands.command(pass_context=True)
     async def deposit(self, ctx):
         """Shows wallet info"""
         port =  "11311"
         account = str(ctx.message.author)
-        user_addy = rpc.getaddressesbyaccount(self, account)
+        user_addy = self.rpc.getaddressesbyaccount(account)
         await self.bot.say(params.mention+"'s Deposit Address: `"+str(user_addy[0])+"`")
 
 def setup(bot):
