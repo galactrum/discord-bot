8dbe82eaa9effa7bcb86c56012c4e0f5e5bd90cd
Aareon <askully13@gmail.com>
2017-02-08 01:14:07 -0800

    Attempt to make RPC a module for use in deposit.py

--- deposit.py
+++ deposit.py
@@ -1,38 +1,20 @@
 import discord, json, requests
 from discord.ext import commands
+from utils import rpc_module as rpc
 
-def rpcdat(method,params,port):
-	try:
-		rpcdata = json.dumps({
-			"jsonrpc": 1.0,
-			"id":"rpctest",
-			"method": str(method),
-			"params": params,
-			"port": port
-			})
-		req = requests.get('http://127.0.0.1:'+port, data=rpcdata, auth=('user', 'pass'), timeout=8)
-		return req.json()['result']
-	except Exception as e:
-		return "Error: "+str(e)
+rpc = rpc.Rpc
 
 class deposit:
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command(pass_context=True)
-	async def deposit(self, ctx):
-		"""Shows wallet info"""
-		port =  "11311"
-		params = ctx.message.author
-		user_addy = rpcdat('getaddressesbyaccount',[str(params)],port)
-		try:
-			if user_addy[0] == "":
-				new_user_addy = rpcdat('',)
-			else:
-				await self.bot.say(params.mention+"'s Deposit Address: `"+str(user_addy[0])+"`")
-		except IndexError:
-			user_addy = rpcdat('getaccountaddress',[str(params)],port)
-			await self.bot.say(params.mention+"'s Deposit Address: `"+str(user_addy)+"`")
+    @commands.command(pass_context=True)
+    async def deposit(self, ctx):
+        """Shows wallet info"""
+        port =  "11311"
+        account = ctx.message.author
+        user_addy = rpc.getaddressesbyaccount(account)
+        await self.bot.say(params.mention+"'s Deposit Address: `"+str(user_addy[0])+"`")
 
 def setup(bot):
-	bot.add_cog(deposit(bot))
+    bot.add_cog(deposit(bot))
