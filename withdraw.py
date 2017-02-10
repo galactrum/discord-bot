2b1989fc6997615145a79014a160b5da16baf4cd
Aareon <askully13@gmail.com>
2017-02-09 19:29:13 -0800

    Use rpc_module

--- withdraw.py
+++ withdraw.py
@@ -1,33 +1,11 @@
 import discord, json, requests, pymysql.cursors
 from discord.ext import commands
-
-class rpc:
-
-    def __init__(self):
-        self.port = "11311"
-        self.rpc_user = 'srf2UUR0'
-        self.rpc_pass = 'srf2UUR0XomxYkWw'
-        self.serverURL = 'http://localhost:'+self.port
-        self.headers = {'content-type': 'application/json'}
-
-    def listtransactions(self,params,count):
-        payload = json.dumps({"method": "listtransactions", "params": [params,count], "jsonrpc": "2.0"})
-        response = requests.get(self.serverURL, headers=self.headers, data=payload, auth=(self.rpc_user,self.rpc_pass))
-        return(response.json()['result'])
-
-    def validateaddress(self,params):
-        payload = json.dumps({"method": "validateaddress", "params": [params], "jsonrpc": "2.0"})
-        response = requests.get(self.serverURL, headers=self.headers, data=payload, auth=(self.rpc_user,self.rpc_pass))
-        return(response.json()['result'])
-
-    def withdraw(self, account, address, amount):
-        payload = json.dumps({"method": "sendfrom", "params": [account, address, amount], "jsonrpc": "2.0"})
-        response = requests.get(self.serverURL, headers=self.headers, data=payload, auth=(self.rpc_user,self.rpc_pass))
-        return(response.json()['result'])
+from cogs.utils import rpc_module as rpc
 
 class withdraw:
     def __init__(self, bot):
         self.bot = bot
+        self.rpc = rpc.Rpc()
 
         self.connection = pymysql.connect(
             host='localhost',
