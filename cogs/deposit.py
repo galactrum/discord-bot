9a6997ca92d6a5d176869ebc28627ce97ccaa218
Jacob Culley <jacob@culley.me>
2017-02-13 20:13:12 +0000

    please

--- cogs/deposit.py
+++ cogs/deposit.py
@@ -1,18 +1,18 @@
 import discord, json, requests
 from discord.ext import commands
-from utils import rpc_module
+from utils import rpc_module as rpc
 
-rpc = rpc_module.Rpc()
 
 class Deposit:
     def __init__(self, bot):
         self.bot = bot
+        self.rpc = rpc.Rpc()
 
     @commands.command(pass_context=True)
     async def deposit(self, ctx):
         """Shows wallet info"""
         account = ctx.message.author
-        user_addy = rpc.getaccountaddress(str(account))
+        user_addy = self.rpc.getaccountaddress(str(account))
         await self.bot.say(account.mention + "'s Deposit Address: `" + str(user_addy) + "`")
 
 
