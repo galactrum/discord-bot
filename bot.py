707337cd02de381d7f86f2ea1134803bb0edb465
Aareon <askully13@gmail.com>
2017-02-13 10:40:41 -0800

    Improve readability throughout

--- bot.py
+++ bot.py
@@ -13,19 +13,21 @@ startup_extensions = os.listdir("./cogs")
 if "__pycache__" in startup_extensions:
     startup_extensions.remove("__pycache__")
 
-output.info("Loading {} extension(s)...".format(len(startup_extensions)))
-
 
 @bot.event
 async def on_ready():
+    output.info("Loading {} extension(s)...".format(len(startup_extensions)))
     loaded_extensions = []
+
     for extension in startup_extensions:
         try:
-            bot.load_extension("cogs."+extension.replace(".py",""))
+            bot.load_extension("cogs.{}".format(extension.replace(".py","")))
             loaded_extensions.append(extension)
+
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
             output.error('Failed to load extension {}\n\t->{}'.format(extension, exc))
+
     output.success('Successfully loaded the following extension(s); {}'.format(loaded_extensions))
 
 def is_owner(ctx):
@@ -35,11 +37,13 @@ def is_owner(ctx):
 @commands.check(is_owner)
 async def shutdown(ctx):
     author = str(ctx.message.author)
+
     try:
         await bot.say("Shutting down...")
         await bot.logout()
         bot.loop.stop()
         output.info('{} has shut down the bot...'.format(author))
+
     except Exception as e:
         exc = '{}: {}'.format(type(e).__name__, e)
         output.error('{} has attempted to shut down the bot, but the follwing '
@@ -49,10 +53,12 @@ async def shutdown(ctx):
 @commands.check(is_owner)
 async def load(ctx, module:str):
     author = str(ctx.message.author)
+
     try:
-        bot.load_extension("cogs."+module)
+        bot.load_extension("cogs.{}".format(module))
         output.info('{} loaded module: {}'.format(author, module))
         await bot.say("Successfully loaded {}.py".format(module))
+
     except Exception as e:
         exc = '{}: {}'.format(type(e).__name__, e)
         output.error('{} attempted to load module \'{}\' but the following '
@@ -64,10 +70,12 @@ async def load(ctx, module:str):
 @commands.check(is_owner)
 async def unload(ctx, module:str):
     author = str(ctx.message.author)
+
     try:
-        bot.unload_extension("cogs."+module)
+        bot.unload_extension("cogs.{}".format(module))
         output.info('{} unloaded module: {}'.format(author, module))
         await bot.say("Successfully unloaded {}.py".format(module))
+
     except Exception as e:
         exc = '{}: {}'.format(type(e).__name__, e)
         await bot.say('Failed to load extension {}\n\t->{}'.format(module, exc))
