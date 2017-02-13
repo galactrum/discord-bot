95a6abf5dd9a9b95781e9832e760029426fbb9dd
OrangeChaten <josejoshua@hotmail.co.uk>
2017-02-13 22:09:28 +0000

    small change

--- cogs/pull.py
+++ cogs/pull.py
@@ -8,7 +8,8 @@ class Pull:
     @commands.command(pass_context=True)
     async def pull(self, ctx):
         await self.bot.say("Pulling...")
-        os.system("git pull")
+        returned = os.system("git pull")
+        await self.bot.say(":+1:Returned code "+ returned)
 
 def setup(bot):
     bot.add_cog(Pull(bot))
