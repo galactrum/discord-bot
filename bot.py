168117bb6276a97414c0c8e96e16fa00dea61972
Aareon <askully13@gmail.com>
2017-02-13 07:41:22 -0800

    Fix exception for __pycache__ being loaded

--- bot.py
+++ bot.py
@@ -7,8 +7,10 @@ description = '''Netcoin tip bot'''
 bot = commands.Bot(command_prefix='!', description=description)
 
 startup_extensions = os.listdir("./cogs")
+if "__pycache__" in startup_extensions:
+    startup_extensions.remove("__pycache__")
 
-output.info("Loading {} extensions...".format(len(startup_extensions)))
+output.info("Loading {} extension(s)...".format(len(startup_extensions)))
 
 
 @bot.event
@@ -20,8 +22,8 @@ async def on_ready():
             loaded_extensions.append(extension)
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
-            output.error('Failed to load extension {}\n{}'.format(extension, exc))
-    output.success("Successfully loaded the following extensions; "+str(loaded_extensions))
+            output.error('Failed to load extension {}\n\t->{}'.format(extension, exc))
+    output.success("Successfully loaded the following extension(s); "+str(loaded_extensions))
 
 with open("config.json",'r') as f:
     config = json.loads(f.read())            
