7bec3c589501077a8e7ec7b31c6c0d5a984294fb
Aareon <askully13@gmail.com>
2017-02-13 21:13:05 -0800

    Fix bad message

--- cogs/test.py
+++ cogs/test.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 
-
-@commands.command(pass_context=True)
-async def test(self, ctx):
-    await self.bot.say(str(ctx.message.Message))
+class Test:
+    @commands.command(pass_context=True)
+    async def test(self, ctx):
+        await self.bot.say(str(ctx.message.Message))
 
 
 def setup(bot):
-    bot.add_cog(test(bot))
+    bot.add_cog(Test(bot))
