b6e1d44fa4410ef054fc63bcd9da0e23dc58eaf7
Aareon <askully13@gmail.com>
2017-02-13 21:24:59 -0800

    Add class, instantiate bot

--- cogs/test.py
+++ cogs/test.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
+
 class Test:
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command(pass_context=True)
     async def test(self, ctx):
         await self.bot.say(str(ctx.message.Message))
