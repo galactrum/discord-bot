69987f65c0c22d7f7a21d29538371cc4ec34e991
Aareon <askully13@gmail.com>
2017-02-15 10:54:32 -0800

    Temp remove check owner

--- cogs/wallet_info.py
+++ cogs/wallet_info.py
@@ -8,7 +8,6 @@ class Wallet:
         self.bot = bot
         self.rpc = rpc.Rpc()
 
-    @commands.check(checks.is_owner)
     @commands.command()
     async def wallet(self):
         """Shows wallet info"""
