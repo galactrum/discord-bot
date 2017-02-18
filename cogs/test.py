55470551475f6cce5fe482345144cd812111be9c
Aareon <askully13@gmail.com>
2017-02-18 13:53:14 -0800

    test things

--- cogs/test.py
+++ cogs/test.py
@@ -8,7 +8,7 @@ class Test:
 
     @commands.command(pass_context=True)
     async def test(self, ctx):
-        await self.bot.say(":tada:{}:tada:".format(ctx.message.author.id))
+        await self.bot.say("!balance")
 
 
 def setup(bot):
