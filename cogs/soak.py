c3dc56b5ca7adfcdf63f76b04838e0fe80f9c6e9
Jacob Culley <jacob@culley.me>
2017-02-19 15:04:30 +0000

    Create users if they don't exist during soak

--- cogs/soak.py
+++ cogs/soak.py
@@ -10,7 +10,7 @@ class Soak:
     def __init__(self, bot):
         self.bot = bot
 
-    async def parse_part_bal(self,result_set,snowflake, name):
+    async def parse_part_bal(self, result_set, snowflake, name):
         params = snowflake
         count = 1000
         get_transactions = rpc.listtransactions(params,count)
@@ -35,9 +35,9 @@ class Soak:
         # Updates balance
         # and return a tuple consisting of the snowflake, and their balance
 
-    async def parse_whole_bal(self,snowflake):
+    async def parse_whole_bal(self, snowflake):
         count = 1000
-        get_transactions = rpc.listtransactions(snowflake,count)
+        get_transactions = rpc.listtransactions(snowflake, count)
         i = len(get_transactions)-1
 
         if len(get_transactions) == 0:
@@ -56,10 +56,10 @@ class Soak:
                     break
             db_bal = new_balance
             Mysql.update_db(snowflake, db_bal, lasttxid)
-            return (snowflake, db_bal)
+            return snowflake, db_bal
 
     @commands.command(pass_context=True)
-    async def soak(self, ctx, amount:float):
+    async def soak(self, ctx, amount: float):
         """Tip all online users"""
         snowflake = ctx.message.author.id
         name = ctx.message.author
@@ -78,17 +78,18 @@ class Soak:
         if float(result_set["balance"]) < amount:
             await self.bot.say("{} **:warning:You cannot tip more money than you have!:warning:**".format(name.mention))
             return
-            
-        for user in online_users:
-            tip_user = user.id
-            tip_user_addy = rpc.getaccountaddress(tip_user)
 
-            amount_split = int(amount)/len(online_users)
-            rpc.sendfrom(snowflake, tip_user_addy, amount_split)
+        amount_split = int(amount) / len(online_users)
+
+        for user in online_users:
+            Mysql.check_for_user(user, user.id)
+            address = rpc.getaccountaddress(user.id)
+            rpc.sendfrom(snowflake, address, amount_split)
         
         await self.parse_part_bal(result_set, snowflake, name)
         await self.bot.say("{} **Soaked {} NET on {} [{}] :money_with_wings:**".format(
-name.mention,str(amount_split), ', '.join([x.mention for x in online_users]), str(amount)))
+            name.mention, str(amount_split), ', '.join([x.mention for x in online_users]), str(amount)))
+
 
 def setup(bot):
     bot.add_cog(Soak(bot))
