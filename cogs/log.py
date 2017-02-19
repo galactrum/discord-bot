b6fbaf729843c0e074aa6ac44e5cf24899c0070a
Jacob Culley <jacob@culley.me>
2017-02-19 18:28:39 +0000

    negatives are bad

--- cogs/log.py
+++ cogs/log.py
@@ -15,7 +15,9 @@ class Log:
         with open(config["file"], "r") as f:
             text = f.readlines()
         length = len(text)
-        if num_lines > length:
+        if num_lines < 1:
+            num_lines = 10
+        elif num_lines > length:
             num_lines = length
         send = "```"
         for line in itertools.islice(text, length - num_lines, length):
