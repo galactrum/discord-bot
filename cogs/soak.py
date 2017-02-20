3125e002c303ac12d6b85617a0e29b32410f7942
Aareon <askully13@gmail.com>
2017-02-20 12:38:32 -0800

    Perform debugging on making new method call

--- cogs/soak.py
+++ cogs/soak.py
@@ -91,12 +91,11 @@ class Soak:
 
         amount_split = int(amount) / len(online_users)
 
-        payments = {}
         for user in online_users:
             address = rpc.getaccountaddress(user.id)
-            payments[address] = str(amount_split)
-        payments = ''.join(json.loads(str(payments)))
-        call = rpc.sendmany(snowflake, payments)
+            json_payments = {str(address): float(amount_split)}
+        print(json_payments)
+        call = rpc.sendmany(snowflake, json_payments)
         print(call)
         
         await self.parse_part_bal(result_set, snowflake, name)
