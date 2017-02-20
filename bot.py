6acdafd34e22f54eee34c3abd8aa250c91c43ba5
Aareon <askully13@gmail.com>
2017-02-20 01:39:45 -0800

    Revert changes to test

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ async def restart(ctx):
     try:
         await bot.say("Restarting...")
         await bot.logout()
-        bot.loop.close()
+        bot.loop.stop()
         output.info('{} has restarted the bot...'.format(author))
         os.system('sudo sh restart.sh')
 
