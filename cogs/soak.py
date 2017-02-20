601a725e5e0257f6a720721ac999e0eccbba0ab4
Aareon <askully13@gmail.com>
2017-02-20 12:12:21 -0800

    Perform debugging on making new method call

--- cogs/soak.py
+++ cogs/soak.py
@@ -95,7 +95,7 @@ class Soak:
         for user in online_users:
             address = rpc.getaccountaddress(user.id)
             payments[address] = str(amount_split)
-        print(payments)
+        print(str(payments))
         call = rpc.sendmany(snowflake, payments)
         print(str(call))
         
