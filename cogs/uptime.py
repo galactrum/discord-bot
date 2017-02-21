5038eadf8d9873286a8830cdbb0486b01b6142af
Jacob Culley <jacob@culley.me>
2017-02-21 20:14:45 +0000

    Rubbish uptime command, resets on reload

---
+++ cogs/uptime.py
@@ -0,0 +1,20 @@
+import discord, datetime, time
+from discord.ext import commands
+
+start_time = time.time()
+
+
+class Uptime:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def uptime(self, ctx):
+        current_time = time.time()
+        difference = current_time - start_time
+        text = str(datetime.timedelta(seconds=difference))
+        await self.bot.say("Current uptime:", text)
+
+
+def setup(bot):
+    bot.add_cog(Uptime(bot))
