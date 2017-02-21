3e71890e425b74d14367fcdc88b7daaeb627d168
Jacob Culley <jacob@culley.me>
2017-02-21 20:28:38 +0000

    improved uptime command

--- cogs/uptime.py
+++ cogs/uptime.py
@@ -11,9 +11,15 @@ class Uptime:
     @commands.command(pass_context=True)
     async def uptime(self, ctx):
         current_time = time.time()
-        difference = current_time - start_time
+        difference = int(round(current_time - start_time))
         text = str(datetime.timedelta(seconds=difference))
-        await self.bot.say("Current uptime: " + text)
+        embed = discord.Embed(colour=ctx.message.author.top_role.colour)
+        embed.add_field(name="Uptime", value=text)
+        embed.set_footer(text="Sponsored by altcointrain.com - Choo!!! Choo!!!")
+        try:
+            await self.bot.say(embed=embed)
+        except discord.HTTPException:
+            await self.bot.say("Current uptime: " + text)
 
 
 def setup(bot):
