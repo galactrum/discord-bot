2c305761cba3b2404e3732e62eabdeb605877280
listonjesse <listonjesse@yahoo.com>
2018-01-24 10:02:09 -0800

    remove name from database because it is unused

--- cogs/withdraw.py
+++ cogs/withdraw.py
@@ -14,8 +14,7 @@ class Withdraw:
     @commands.command(pass_context=True)
     async def withdraw(self, ctx, address: str, amount: float):
         """Withdraw coins from your account to any Phore address"""
-        snowflake = ctx.message.author.id
-        name = ctx.message.author.name      
+        snowflake = ctx.message.author.id    
         if amount <= 0.0:
             await self.bot.say("{} **:warning:You cannot withdraw <= 0!:warning:**".format(ctx.message.author.mention))
             return
@@ -25,7 +24,7 @@ class Withdraw:
             await self.bot.say(":warning:**Invalid amount!**:warning:")
             return
 
-        mysql.check_for_user(name, snowflake)
+        mysql.check_for_user(snowflake)
 
         conf = rpc.validateaddress(address)
         if not conf["isvalid"]:
