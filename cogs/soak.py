effa7adc036972fdd5e57910d11b2f346b0f1dff
Jesse Liston <jeliston@microsoft.com>
2018-01-26 09:24:18 -0800

    Feature/set max users to soak via config (#4)
    *   added soak to config and support for this in the soak cog
    
    * added min_received to soak config to set the lowest a user could receive from a soak. allow enable/disable of soak config values via config

--- cogs/soak.py
+++ cogs/soak.py
@@ -1,7 +1,8 @@
 import math
 import discord
 from discord.ext import commands
-from utils import rpc_module, mysql_module, checks
+from utils import rpc_module, mysql_module, checks, parsing
+import random
 
 rpc = rpc_module.Rpc()
 mysql = mysql_module.Mysql()
@@ -10,23 +11,27 @@ mysql = mysql_module.Mysql()
 class Soak:
     def __init__(self, bot):
         self.bot = bot
+        soak_config = parsing.parse_json('config.json')['soak']
+        '''
+        soak_max_recipients sets the max recipients for a soak. chosen randomly.
+        soak_min_received sets the minimum possible soak received for a user.
+        The number of soak recipients will be adjusted to fit these constraints
+        if enabled via use_max_recipients and use_min_received
+        '''
+        self.soak_max_recipients = soak_config["soak_max_recipients"]
+        self.use_max_recipients = soak_config["use_max_recipients"]
+        self.soak_min_received = soak_config["soak_min_received"]
+        self.use_min_received = soak_config["use_min_received"]
 
     @commands.command(pass_context=True)
     @commands.check(checks.allow_soak)
     async def soak(self, ctx, amount: float):
         """Tip all online users"""
-        snowflake = ctx.message.author.id
-
-        online_users = [x for x in ctx.message.server.members if x.status == discord.Status.online]
-        if ctx.message.author in online_users:
-            online_users.remove(ctx.message.author)
-        for user in online_users:
-            if user.bot:
-                online_users.remove(user)
-
-        if len(online_users) == 0:
-            await self.bot.say("{}, you are all alone if you don't include bots! Trying soaking when people are online.".format(ctx.message.author.mention))
+        if self.use_max_recipients and self.soak_max_recipients == 0:
+            await self.bot.say("**:warning: max users for soak is set to 0! Talk to the config owner. :warning:**")
             return
+
+        snowflake = ctx.message.author.id
 
         mysql.check_for_user(snowflake)
         balance = mysql.get_balance(snowflake, check_update=True)
@@ -35,14 +40,57 @@ class Soak:
             await self.bot.say("{} **:warning:You cannot tip more money than you have!:warning:**".format(ctx.message.author.mention))
             return
 
-        amount_split = math.floor(float(amount) * 1e8 / len(online_users)) / 1e8
+        online_users = [x for x in ctx.message.server.members if x.status == discord.Status.online]
+        if ctx.message.author in online_users:
+            online_users.remove(ctx.message.author)
 
         for user in online_users:
+            if user.bot:
+                online_users.remove(user)
+
+        if self.use_max_recipients:
+            len_receivers = min(len(online_users), self.soak_max_recipients)
+        else:
+            len_receivers = len(online_users)
+
+        if self.use_min_received:
+            len_receivers = min(len_receivers, amount / self.soak_min_received)
+
+        if len_receivers == 0:
+            await self.bot.say("{}, you are all alone if you don't include bots! Trying soaking when people are online.".format(ctx.message.author.mention))
+            return
+
+        amount_split = math.floor(float(amount) * 1e8 / len_receivers) / 1e8
+        if amount_split == 0:
+            await self.bot.say("{} **:warning:{} is not enough to split between {} users:warning:**".format(amount, len_receivers))
+            return
+
+        for i in len_receivers:
+            user = random.choice(online_users)
+            online_users.remove(user)
             mysql.check_for_user(user.id)
             mysql.add_tip(snowflake, user.id, amount_split)
 
-        await self.bot.say("{} **Soaked {} PHR on {} [{}] :money_with_wings:**".format(ctx.message.author.mention, str(amount_split), ', '.join([x.mention for x in online_users]), str(amount)))
+        long_soak_msg = "{} **Soaked {} PHR on {} [{}] :money_with_wings:**".format(ctx.message.author.mention, str(amount_split), ', '.join([x.mention for x in online_users]), str(amount))
 
+        if len(long_soak_msg) > 2000:
+            await self.bot.say("{} **Soaked {} PHR on {} users [{}] :money_with_wings:**".format(ctx.message.author.mention, str(amount_split), len_receivers, str(amount)))
+        else:
+            await self.bot.say(long_soak_msg)
+
+    @commands.command()
+    async def soak_info(self):        
+        if self.use_max_recipients:
+            st_max_users = str(self.soak_max_recipients)
+        else:
+            st_max_users = "<disabled>"
+
+        if self.use_min_received:
+            st_min_received = str(self.soak_min_received)
+        else:
+            st_min_received = "<disabled>"
+            
+        await self.bot.say("Soak info: max recipients {}, min amount receivable {}".format(st_max_users, st_min_received))
 
 def setup(bot):
     bot.add_cog(Soak(bot))
